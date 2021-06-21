e843fc9161d40c284e16be86e9a8c7f8144f3148
Nunny <nunnyreyes@gmail.com>
2021-06-21 13:15:52 -0400

    adding comment

---
+++ app.js
@@ -0,0 +1,5 @@
+//note
+//v-bind dynamically binds an attribute(like src) 
+// to an expression. In binding we are directly 
+// linking to the source of data. Usually this 
+// means the data object inside of the Vue instance.
